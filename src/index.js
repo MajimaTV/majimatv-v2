55e30b7e813881c550b13cafa90037ad0ee197b0
MajimaTV <kingow8899@gmail.com>
2025-04-23 12:21:39 +0900

    Create index.js
    Add initial React entry point in src/index.js
    Includes root rendering with ReactDOM and MajimaTV component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MajimaTV from "./MajimaTV";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MajimaTV />
+  </React.StrictMode>
+);
